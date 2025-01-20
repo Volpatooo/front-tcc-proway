2c99389d5057aab25e99240680cbe23df62c60ee
Mateus Volpato Cirico <teteuvolpato@gmail.com>
2025-01-20 13:20:54 -0300

    Começo componente cadastro cliente

--- front-tcc/src/app/cadastro-cliente/cadastro-cliente.component.ts
+++ front-tcc/src/app/cadastro-cliente/cadastro-cliente.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { MenuComponent } from '../shred/menu/menu.component';
+import { FooterComponent } from '../shred/footer/footer.component';
 
 @Component({
   selector: 'app-cadastro-cliente',
-  imports: [],
+  imports: [
+    MenuComponent,
+    FooterComponent
+  ],
   templateUrl: './cadastro-cliente.component.html',
   styleUrl: './cadastro-cliente.component.scss'
 })
